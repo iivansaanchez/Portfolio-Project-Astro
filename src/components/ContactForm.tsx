8d0eb595c0292183b5f2d9c506a375bbff91649d
iivansaanchez <ivansanchezsanchez2002@gmail.com>
2024-12-03 14:55:37 +0100

    Pequeños cambios en diseño

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -47,101 +47,127 @@ const ContactForm: React.FC = () => {
   };
 
   return (
-    
-      <div className="w-full max-w-3xl p-8 bg-white rounded-lg shadow-xl shadow-yellow-500/50">
-        <form
-          className="flex flex-wrap justify-between gap-4 mb-4"
-          onSubmit={handleSubmit}
-        >
-          {/* Input Name */}
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={formData.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={`flex-grow px-6 py-3 bg-gray-300 text-black rounded focus:outline-none hover:bg-gray-400 ${
-              errors.name ? "border-2 border-red-500" : ""
-            }`}
-            required
-          />
-
-          {/* Input Phone */}
-          <input
-            type="text"
-            name="phone"
-            placeholder="Phone"
-            value={formData.phone}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={`flex-grow px-6 py-3 bg-gray-300 text-black rounded focus:outline-none hover:bg-gray-400 ${
-              errors.phone ? "border-2 border-red-500" : ""
-            }`}
-            required
-          />
-
-          {/* Input Email */}
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={formData.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={`flex-grow px-6 py-3 bg-gray-300 text-black rounded focus:outline-none hover:bg-gray-400 ${
-              errors.email ? "border-2 border-red-500" : ""
-            }`}
-            required
-          />
-
-          {/* Input Message */}
-          <input
-            type="text"
-            name="message"
-            placeholder="Message"
-            value={formData.message}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={`flex-grow px-6 py-3 bg-gray-300 text-black rounded focus:outline-none hover:bg-gray-400 ${
-              errors.message ? "border-2 border-red-500" : ""
-            }`}
-            required
-          />
-
-          {/* Submit Button */}
-          <button
-            type="submit"
-            className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-700 self-stretch"
+    <div className="w-full max-w-3xl mx-auto p-8 bg-white rounded-lg shadow-xl shadow-blue-500/50">
+      {/* Encabezado estilizado */}
+      <div className="flex items-center justify-center gap-4 mb-8">
+        <div className="bg-blue-500 text-white w-12 h-12 flex items-center justify-center rounded-full shadow-lg">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
-            CONTACT US
-          </button>
-        </form>
-
-        {/* Contact Information with image */}
-        <div className="flex items-start justify-between">
-          <ul className="text-left text-black text-xl font-normal">
-            <li className="mb-2">
-              Email:{" "}
-              <a href="mailto:sansaniva357@gmail.com" className="text-blue-600">
-              sansaniva357@gmail.com
-              </a>
-            </li>
-            <li className="mb-2">Phone: +34 123 456 789</li>
-            <li className="mb-2">
-              LinkedIn:{" "}
-              <a
-                href="https://www.linkedin.com/IvánSánchezSánchez"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-600"
-              >
-                https://www.linkedin.com/IvánSánchezSánchez
-              </a>
-            </li>
-          </ul>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M8 12h4m-2 0v-2m0 4v-2m10-4H6a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V8a2 2 0 00-2-2z"
+            />
+          </svg>
         </div>
+        <h1 className="text-3xl font-extrabold text-gray-800">Contact me</h1>
       </div>
+
+      {/* Formulario */}
+      <form
+        className="flex flex-wrap justify-between gap-4 mb-6"
+        onSubmit={handleSubmit}
+      >
+        {/* Input Name */}
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className={`flex-grow px-6 py-3 bg-gray-300 text-black rounded focus:outline-none hover:bg-gray-400 ${
+            errors.name ? "border-2 border-red-500" : ""
+          }`}
+          required
+        />
+
+        {/* Input Phone */}
+        <input
+          type="text"
+          name="phone"
+          placeholder="Phone"
+          value={formData.phone}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className={`flex-grow px-6 py-3 bg-gray-300 text-black rounded focus:outline-none hover:bg-gray-400 ${
+            errors.phone ? "border-2 border-red-500" : ""
+          }`}
+          required
+        />
+
+        {/* Input Email */}
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className={`flex-grow px-6 py-3 bg-gray-300 text-black rounded focus:outline-none hover:bg-gray-400 ${
+            errors.email ? "border-2 border-red-500" : ""
+          }`}
+          required
+        />
+
+        {/* Input Message */}
+        <input
+          type="text"
+          name="message"
+          placeholder="Message"
+          value={formData.message}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          className={`flex-grow px-6 py-3 bg-gray-300 text-black rounded focus:outline-none hover:bg-gray-400 ${
+            errors.message ? "border-2 border-red-500" : ""
+          }`}
+          required
+        />
+
+        {/* Submit Button */}
+        <button
+          type="submit"
+          className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-700 self-stretch transition-all duration-300"
+        >
+          CONTACT US
+        </button>
+      </form>
+
+      {/* Información de contacto */}
+      <div className="flex items-start justify-between">
+        <ul className="text-left text-gray-700 text-lg font-medium">
+          <li className="mb-2">
+            <span className="font-semibold">Email:</span>{" "}
+            <a
+              href="mailto:sansaniva357@gmail.com"
+              className="text-blue-600 hover:underline"
+            >
+              sansaniva357@gmail.com
+            </a>
+          </li>
+          <li className="mb-2">
+            <span className="font-semibold">Phone:</span> +34 123 456 789
+          </li>
+          <li className="mb-2">
+            <span className="font-semibold">LinkedIn:</span>{" "}
+            <a
+              href="https://www.linkedin.com/IvánSánchezSánchez"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              https://www.linkedin.com/IvánSánchezSánchez
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,7 +21,6 @@ const Projects = () => {
 
   const url: string = "http://localhost:8080/api/v1/projects"; // API URL
 
-  // Make the request when the page or search term changes
   useEffect(() => {
     fetchData(page, searchTerm);
   }, [page, searchTerm]);
@@ -39,7 +38,6 @@ const Projects = () => {
     setTotalPages(data.totalPages || 1);
   };
 
-  // Function to delete a project
   const handleDelete = async (id: number) => {
     const response = await fetch(`${url}/${id}`, {
       method: 'DELETE',
@@ -52,92 +50,94 @@ const Projects = () => {
     }
   };
 
-  // Function to determine the color of the status
   const getStatusColor = (status: string) => {
     switch (status) {
       case 'In Development':
-        return 'bg-yellow-400'; // Yellow color
+        return 'bg-yellow-400';
       case 'Testing':
-        return 'bg-blue-400'; // Blue color
+        return 'bg-blue-400';
       case 'Completed':
-        return 'bg-green-400'; // Green color
+        return 'bg-green-400';
       default:
-        return 'bg-gray-400'; // Gray color if the status is not defined
+        return 'bg-gray-400';
     }
   };
 
-  const ProjectCard = ({ project, test = false }: { project: Project; test?: boolean }) => {
-    return (
-      <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-105 flex flex-col justify-center items-center">
-        {/* Representative project image */}
-        <div className="w-full h-40 bg-gray-300 rounded-t-lg flex items-center justify-center">
-          <img
-            src="https://via.placeholder.com/400x200.png?text=Project+Image"
-            alt="Project Image"
-            className="w-full h-full object-cover rounded-t-lg"
-          />
-        </div>
-
-        {/* Project information */}
-        <div className="mt-4 w-full text-center">
-          <h2 className="text-2xl font-semibold text-gray-800">{project.nameProject}</h2>
-          
-          {/* Status with background color */}
-          <p className={`mt-2 text-white px-4 py-2 rounded-full inline-block ${getStatusColor(project.status)}`}>
-            {project.status}
-          </p>
-
-          <div className="mt-4 w-full">
-            <p className="font-semibold text-gray-600">Technologies:</p>
-            <div className="flex flex-wrap justify-center gap-2 mt-2">
-              {project.listTechnologies.map((technology) => (
-                <span
-                  key={technology.id}
-                  className="bg-blue-500 text-white px-4 py-1 rounded-full text-sm"
-                >
-                  {technology.name}
-                </span>
-              ))}
-            </div>
-          </div>
-
-          {test && (
-            <button
-              onClick={() => handleDelete(project.id)}
-              className="mt-6 bg-red-500 text-white px-6 py-3 rounded-md hover:bg-red-600 transition-all duration-300"
-            >
-              Delete
-            </button>
-          )}
-        </div>
+  const ProjectCard = ({ project, test = false }: { project: Project; test?: boolean }) => (
+    <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-105 flex flex-col justify-center items-center">
+      <div className="w-full h-40 bg-gray-300 rounded-t-lg flex items-center justify-center">
+        <img
+          src="https://via.placeholder.com/400x200.png?text=Project+Image"
+          alt="Project Image"
+          className="w-full h-full object-cover rounded-t-lg"
+        />
       </div>
-    );
-  };
+      <div className="mt-4 w-full text-center">
+        <h2 className="text-2xl font-semibold text-gray-800">{project.nameProject}</h2>
+        <p className={`mt-2 text-white px-4 py-2 rounded-full inline-block ${getStatusColor(project.status)}`}>
+          {project.status}
+        </p>
+        <div className="mt-4 w-full">
+          <p className="font-semibold text-gray-600">Technologies:</p>
+          <div className="flex flex-wrap justify-center gap-2 mt-2">
+            {project.listTechnologies.map((technology) => (
+              <span key={technology.id} className="bg-blue-500 text-white px-4 py-1 rounded-full text-sm">
+                {technology.name}
+              </span>
+            ))}
+          </div>
+        </div>
+        {test && (
+          <button
+            onClick={() => handleDelete(project.id)}
+            className="mt-6 bg-red-500 text-white px-6 py-3 rounded-md hover:bg-red-600 transition-all duration-300"
+          >
+            Delete
+          </button>
+        )}
+      </div>
+    </div>
+  );
 
   return (
     <>
-      {/* Centered search bar */}
+      {/* Encabezado estilizado */}
+      <div className="flex items-center justify-center gap-4 mb-12">
+        <div className="bg-purple-500 text-white w-12 h-12 flex items-center justify-center rounded-full shadow-lg">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12l-4-4m0 8l4-4m0 0H4" />
+          </svg>
+        </div>
+        <h1 className="text-3xl font-extrabold text-gray-800">Projects</h1>
+      </div>
+
+      {/* Barra de búsqueda */}
       <div className="mb-6 flex justify-center">
         <input
           type="text"
           placeholder="Search by name"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          onKeyDown={(e) => e.key === 'Enter' && fetchData(page, searchTerm)} // Search when pressing "Enter"
+          onKeyDown={(e) => e.key === 'Enter' && fetchData(page, searchTerm)}
           className="w-full max-w-xs p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300"
         />
       </div>
 
-      {/* Centered project list */}
+      {/* Lista de proyectos */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-center items-center mb-12">
         {posts.map((project) => (
           <ProjectCard key={project.nameProject} project={project} test={true} />
         ))}
       </div>
 
-      {/* More styled pagination */}
+      {/* Paginación */}
       <div className="mt-8 flex justify-center items-center space-x-6">
-        {/* Previous page button */}
         <button
           onClick={() => setPage(page - 1)}
           disabled={page === 0}
@@ -145,27 +145,22 @@ const Projects = () => {
         >
           <span className="text-2xl font-bold">&larr;</span>
         </button>
-
-        {/* Pagination with page numbers */}
         <div className="flex items-center space-x-2">
           {[page - 1, page, page + 1].map((pageNumber) => {
             if (pageNumber < 0 || pageNumber >= totalPages) return null;
-
             return (
               <button
                 key={pageNumber}
                 onClick={() => setPage(pageNumber)}
-                className={`w-12 h-12 flex items-center justify-center rounded-full text-white font-semibold
-                            ${page === pageNumber ? 'bg-blue-600' : 'bg-blue-500'}
-                            hover:bg-blue-700 transition-all duration-300 transform hover:scale-110`}
+                className={`w-12 h-12 flex items-center justify-center rounded-full text-white font-semibold ${
+                  page === pageNumber ? 'bg-blue-600' : 'bg-blue-500'
+                } hover:bg-blue-700 transition-all duration-300 transform hover:scale-110`}
               >
                 {pageNumber + 1}
               </button>
             );
           })}
         </div>
-
-        {/* Next page button */}
         <button
           onClick={() => setPage(page + 1)}
           disabled={page === totalPages - 1}
